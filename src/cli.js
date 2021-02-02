595fc5c555978a8f5a173b33b45bdecf0c7e197b
Denis Simonenko <delf.09@mail.ru>
2021-02-02 17:27:08 +0900

    fix brain-games

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 const name = () => {
   const yourName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + yourName + '!');
+  console.log(`Hello, ${yourName}!`);
 };
 export default name;
