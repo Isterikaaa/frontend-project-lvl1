f027b129efbab3b3832561507509524651cd5f75
Denis Simonenko <delf.09@mail.ru>
2021-04-01 19:45:33 +0900

    fix calc.js and even.js

--- src/even.js
+++ src/even.js
@@ -8,21 +8,22 @@ const randomNumber = (min, max) => {
 const isEvenNumber = (num) => (num % 2 === 0) ? 'yes' : 'no';
 
 const playRounds = () => {
-    const name = greeting();
-    console.log('Answer "yes" if the number is even, otherwise answer "no".')
-    for (let currentAnswers = 0; currentAnswers < 3;) {
-        let randomNum = randomNumber(1, 100);
-        console.log(`Question: ${randomNum}`);
-        let correctAnswer = isEvenNumber(randomNum);
-        let playerAnswer = readlineSync.question('Your answer: ');
-        if (correctAnswer === playerAnswer) {
-            console.log('Correct!');
-            currentAnswers += 1;
+  const name = greeting();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  for (let currentAnswers = 0; currentAnswers < 3;) {
+    const randomNum = randomNumber(1, 100);
+    console.log(`Question: ${randomNum}`);
+    const correctAnswer = isEvenNumber(randomNum);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    if (correctAnswer === playerAnswer) {
+      console.log('Correct!');
+      currentAnswers += 1;
     } else {
-        console.log("yes' is wrong answer ;(. Correct answer was 'no'.");
-        console.log(`Let's try again, ${name}!`);
-        return;
-      } 
+      console.log("yes' is wrong answer ;(. Correct answer was 'no'.");
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
   } console.log(`Congratulations, ${name}!`);
 };
+
 export default playRounds;
