6524de153fd2be6b4b7a04271dee1f3d5393df55
Denis Simonenko <delf.09@mail.ru>
2021-04-01 19:31:52 +0900

    fix calc.js 2

--- src/calc.js
+++ src/calc.js
@@ -15,14 +15,14 @@ const findingARandomSign = () => {
 const calculation = (num1, sign, num2) => {
   switch (sign) {
     case '-':
-    return num1 - num2;
-    break;
+  return num1 - num2;
+  break;
     case '+':
-    return num1 + num2;
-    break;
+  return num1 + num2;
+  break;
     case '*':
-    return num1 * num2;
-    break;
+  return num1 * num2;
+  break;
   }
 };
 
@@ -30,12 +30,12 @@ const playRounds = () => {
   const name = greeting();
   console.log('What is the result of the expression?');
   for (let currentAnswers = 0; currentAnswers < 3;) {
-    let randomNum = randomNumber(1, 25);
-    let randomNum2 = randomNumber(1, 25);
-    let randomSigns = findingARandomSign();
+    const randomNum = randomNumber(1, 25);
+    const randomNum2 = randomNumber(1, 25);
+    const randomSigns = findingARandomSign();
     console.log(`Question: ${randomNum} ${randomSigns} ${randomNum2}`);
-    let correctAnswer = String(calculation(randomNum, randomSigns, randomNum2));
-    let playerAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = String(calculation(randomNum, randomSigns, randomNum2));
+    const playerAnswer = readlineSync.question('Your answer: ');
     if (correctAnswer === playerAnswer) {
       console.log('Correct!');
       currentAnswers += 1;
