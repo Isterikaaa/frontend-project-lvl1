fdcc682199b830b01fb6c792595e1b6facb23831
Denis Simonenko <delf.09@mail.ru>
2021-04-06 21:22:27 +0900

    fix gcd.js

--- src/gcd.js
+++ src/gcd.js
@@ -5,28 +5,28 @@ import randomNumber from './randomNumber.js';
 const nod = (num1, num2) => {
   for (let i = num1; i >= 1; i -= 1) {
     if (num1 % i === 0 && num2 % i === 0) {
-        return i;
-      }
+      return i;
     }
+  }
 };
-  
+
 const playRounds = () => {
   const name = greeting();
   console.log('Find the greatest common divisor of given numbers.');
   for (let currentAnswers = 0; currentAnswers < 3;) {
-    let randomNum = randomNumber(1, 100);
-    let randomNum2 = randomNumber(1, 100);
+    const randomNum = randomNumber(1, 100);
+    const randomNum2 = randomNumber(1, 100);
     console.log(`Question: ${randomNum} ${randomNum2}`);
-    let correctAnswer = String(nod(randomNum, randomNum2));
-    let playerAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = String(nod(randomNum, randomNum2));
+    const playerAnswer = readlineSync.question('Your answer: ');
     if (correctAnswer === playerAnswer) {
       console.log('Correct!');
       currentAnswers += 1;
     } else {
-        console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
-        console.log(`Let's try again, ${name}!`);
-        return;
-    } 
+      console.log(`${playerAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.`);
+      console.log(`Let's try again, ${name}!`);
+      return;
+    }
   } console.log(`Congratulations, ${name}!`);
 };
 export default playRounds;
